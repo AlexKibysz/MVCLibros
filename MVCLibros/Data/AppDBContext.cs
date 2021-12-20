9aecdd4b9b94d8e23778438dc1b16b852e1362e1
Alexander Kibysz <Alexander.Kibysz@gmail.com>
2021-12-19 21:08:47 -0300

    Agregar archivos de proyecto.

---
+++ MVCLibros/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibros.Models;
+
+namespace MVCLibros.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base (options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
